5e43e66034a8203872cd766db352b03b95ec15a1
Sunil Gumatimath <sunil@gumatimath.dev>
2026-04-04 00:26:29 +0530

    feat: add React entry point with StrictMode and ThemeProvider

---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { ThemeProvider } from './ThemeProvider.tsx'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
+
+// TODO: Add Google Analytics integration
+
+// TODO: Add Axios API client with interceptors
